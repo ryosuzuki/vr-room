6113e2a5fdbb5c1294d525e927d80f6d4c6ae912
Ryo Suzuki <ryosuzk@gmail.com>
2019-07-24 12:01:17 -0700

    Add room layout and furniture

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const app = express()
+const server = http.Server(app)
+
+app.use(bodyParser.json())
+app.use('/', express.static(__dirname + '/'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+server.listen(8080, () => {
+  console.log('listening on 8080')
+})
